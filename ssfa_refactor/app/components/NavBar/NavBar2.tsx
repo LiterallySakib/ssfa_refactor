2f092b31f8c3a0830319b2ab452960ed331f0096
Sakib Niaz <sakibniaz221@gmail.com>
2025-12-04 09:28:19 -0500

    decided to change development flow from doing all pages common assets at the same time to do one at a time. The first one is homepage

---
+++ ssfa_refactor/app/components/NavBar/NavBar2.tsx
@@ -0,0 +1,11 @@
+import styles from './NavBar2.css';
+import Image from 'next/image';
+
+const NavBar2 = () => {
+    return(
+        <div>
+        </div>
+    );
+}
+
+export default NavBar2;
---
+++ ssfa_refactor/app/home/page.tsx
@@ -0,0 +1,11 @@
+import NavBar2 from "../components/NavBar/NavBar2";
+
+function HomePage() {
+    return (
+    <div>
+      <NavBar2/>
+    </div>
+    );
+  }
+
+export default HomePage;
